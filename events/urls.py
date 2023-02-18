ddac6b3a97841993b15ca28b68c5c419869ccfbf
MartinaP87 <bleach1987@hotmail.it>
2023-02-18 16:55:19 +0000

    Create events views and URLs.

---
+++ events/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from events import views
+
+urlpatterns = [
+    path('events/', views.EventList.as_view()),
+    path('events/<int:pk>/', views.EventDetail.as_view()),
+    path('events/galleries/', views.GalleryList.as_view()),
+    path('events/galleries/<int:pk>/', views.GalleryDetail.as_view()),
+    path('events/galleries/photos/', views.PhotoList.as_view()),
+    path('events/galleries/photos/<int:pk>/', views.PhotoDetail.as_view()),
+    path('events/genres/', views.EventGenreList.as_view()),
+    path('events/genres/<int:pk>/', views.EventGenreDetail.as_view()),
+]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from onlyevents_drf_api.permissions import IsOwnerOrReadOnly
+from .models import Event, Gallery, Photo, EventGenre
+from .serializers import EventSerializer, GallerySerializer
+from .serializers import PhotoSerializer, PhotoDetailSerializer
+from .serializers import EventGenreSerializer, EventGenreDetailSerializer
 
-# Create your views here.
+
+class EventList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+
+
+class GalleryList(generics.ListAPIView):
+    serializer_class = GallerySerializer
+    queryset = Gallery.objects.all()
+
+
+class GalleryDetail(generics.RetrieveUpdateAPIView):
+    serializer_class = GallerySerializer
+    queryset = Gallery.objects.all()
+
+
+class PhotoList(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = PhotoSerializer
+    queryset = Photo.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PhotoDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = PhotoDetailSerializer
+    queryset = Photo.objects.all()
+
+
+class EventGenreList(generics.ListCreateAPIView):
+    serializer_class = EventGenreSerializer
+    queryset = EventGenre.objects.all()
+
+    def perform_create(self, serializer):
+        if (self.request.user != serializer.validated_data['event'].owner):
+            raise ValidationError(
+                "You cannot add a genre to someone else event")
+        if serializer.validated_data[
+         'event'].category != serializer.validated_data['genre'].category:
+            raise ValidationError(
+                "You can't add a genre of a different event category"
+            )
+        serializer.save()
+
+
+class EventGenreDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = EventGenreDetailSerializer
+    queryset = EventGenre.objects.all()
