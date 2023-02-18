e14dd4fe6848bfc7963af656b28e6425664aa636
MartinaP87 <bleach1987@hotmail.it>
2023-02-18 15:23:07 +0000

    Add profiles and onlyevents_drf_api URL paths, create a custom permission IsOwnerOrReadOnly and apply it in the ProfileDetail view.

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from onlyevents_drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
@@ -11,5 +12,6 @@ class ProfileList(generics.ListAPIView):
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
