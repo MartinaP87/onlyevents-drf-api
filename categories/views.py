5539f804dd60de7ef6beea9781a5550beebd3856
MartinaP87 <bleach1987@hotmail.it>
2023-02-18 15:45:55 +0000

    Create categories views and IsAdminOrReadOnly custom permission.

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import generics
+from drf_api_event.permissions import IsAdminOrReadOnly
+from .models import Category, Genre
+from .serializers import CategorySerializer, GenreSerializer
 
-# Create your views here.
+
+class CategoryList(generics.ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    def perform_create(self, serializer):
+        if self.request.user.is_superuser:
+            return serializer.save()
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class GenreList(generics.ListAPIView):
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
+
+
+class GenreDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminOrReadOnly]
+    serializer_class = GenreSerializer
+    queryset = Genre.objects.all()
