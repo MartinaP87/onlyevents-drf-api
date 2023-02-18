5cbe1d65b72ecc4d007cee821c0892d0e1d3fff3
MartinaP87 <bleach1987@hotmail.it>
2023-02-18 17:54:02 +0000

    Create comments serializers and views.

---
+++ comments/serializers.py
@@ -0,0 +1,33 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
+    class Meta:
+        model = Comment
+        fields = [
+            'id', 'owner', 'profile_id', 'profile_image',
+            'posted_event', 'created_at', 'updated_at', 'content',
+        ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    posted_event is a read only field so that we dont have to set it on
+    each update
+    """
+    posted_event = serializers.ReadOnlyField(source='posted_event.id')
